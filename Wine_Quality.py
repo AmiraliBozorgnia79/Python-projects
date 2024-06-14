331ffbe531313fd8b29824bd3e8702a523c116f4
AmiraliBozorgnia79 <amiralibozorgnia79@gmail.com>
2024-06-14 01:04:39 -0400

    Wine quality

--- Wine_Quality.py
+++ Wine_Quality.py
@@ -8,6 +8,12 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import accuracy_score, confusion_matrix, r2_score,mean_squared_error,f1_score
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import MinMaxScaler
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras import layers
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.activations import linear, relu, sigmoid,softmax
+
 
 df=pd.read_csv('/Users/amiralibozorgnia/Library/CloudStorage/GoogleDrive-amiralibozorgnia79@gmail.com/My Drive/my computer/Python/data set/winequality-red.csv')
 print(df.head())
@@ -21,3 +27,18 @@ print(df['quality'].value_counts())
 fig, ax = plt.subplots(figsize=(10, 10))
 sns.heatmap(df.corr(), annot=True, fmt='.2f',vmin=0,vmax=1,cbar=True,cmap='YlGnBu',square=True)
 plt.show()
+
+# for i,col in enumerate(df.columns):
+#     if col!='quality':
+#      plt.scatter(df[col],df['quality'],label=col)
+#      plt.xlabel(col)
+#      plt.ylabel('quality')
+#      plt.legend()
+#      plt.show()
+
+
+#Normalization
+Scalar=MinMaxScaler()
+Norm_df=pd.DataFrame(Scalar.fit_transform(df),columns=df.columns)
+print(Norm_df['quality'].value_counts())
+#Neural Network
